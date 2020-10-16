7b3d5ba35be1244f74e67e6c5a8d07e30e2b6071
bastien wermeille <bastien.wermeille@gmail.com>
2020-10-16 23:55:59 +0200

    -

--- 308542/tm.c
+++ 308542/tm.c
@@ -361,6 +361,11 @@ struct transaction
     bool is_readonly;
 };
 
+/** Create (i.e. allocate + init) a new shared memory region, with one first non-free-able allocated segment of the requested size and alignment.
+ * @param size  Size of the first shared segment of memory to allocate (in bytes), must be a positive multiple of the alignment
+ * @param align Alignment (in bytes, must be a power of 2) that the shared memory region must support
+ * @return Opaque shared memory region handle, 'invalid_shared' on failure
+**/
 shared_t tm_create(size_t size, size_t align) {
     struct region* region = (struct region*) malloc(sizeof(struct region));
     if (unlikely(!region)) {
@@ -385,6 +390,9 @@ shared_t tm_create(size_t size, size_t align) {
     return region;
 }
 
+/** Destroy (i.e. clean-up + free) a given shared memory region.
+ * @param shared Shared memory region to destroy, with no running transaction
+**/
 void tm_destroy(shared_t shared) {
     struct region* region = (struct region*) shared;
     struct link* allocs = &(region->allocs);
@@ -400,18 +408,35 @@ void tm_destroy(shared_t shared) {
     lock_cleanup(&(region->lock));
 }
 
+/** [thread-safe] Return the start address of the first allocated segment in the shared memory region.
+ * @param shared Shared memory region to query
+ * @return Start address of the first allocated segment
+**/
 void* tm_start(shared_t shared) {
     return ((struct region*) shared)->start;
 }
 
+/** [thread-safe] Return the size (in bytes) of the first allocated segment of the shared memory region.
+ * @param shared Shared memory region to query
+ * @return First allocated segment size
+**/
 size_t tm_size(shared_t shared) {
     return ((struct region*) shared)->size;
 }
 
+/** [thread-safe] Return the alignment (in bytes) of the memory accesses on the given shared memory region.
+ * @param shared Shared memory region to query
+ * @return Alignment used globally
+**/
 size_t tm_align(shared_t shared) {
     return ((struct region*) shared)->align;
 }
 
+/** [thread-safe] Begin a new transaction on the given shared memory region.
+ * @param shared Shared memory region to start a transaction on
+ * @param is_ro  Whether the transaction is read-only
+ * @return Opaque transaction ID, 'invalid_tx' on failure
+**/
 tx_t tm_begin(shared_t shared, bool is_ro) {
     if (is_ro) {
         if (unlikely(!lock_acquire_shared(&(((struct region*) shared)->lock))))
@@ -424,6 +449,11 @@ tx_t tm_begin(shared_t shared, bool is_ro) {
     }
 }
 
+/** [thread-safe] End the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to end
+ * @return Whether the whole transaction committed
+**/
 bool tm_end(shared_t shared, tx_t tx) {
     if (tx == read_only_tx) {
         lock_release_shared(&(((struct region*) shared)->lock));
@@ -433,16 +463,39 @@ bool tm_end(shared_t shared, tx_t tx) {
     return true;
 }
 
+/** [thread-safe] Read operation in the given transaction, source in the shared region and target in a private region.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param source Source start address (in the shared region)
+ * @param size   Length to copy (in bytes), must be a positive multiple of the alignment
+ * @param target Target start address (in a private region)
+ * @return Whether the whole transaction can continue
+**/
 bool tm_read(shared_t shared as(unused), tx_t tx as(unused), void const* source, size_t size, void* target) {
     memcpy(target, source, size);
     return true;
 }
 
+/** [thread-safe] Write operation in the given transaction, source in a private region and target in the shared region.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param source Source start address (in a private region)
+ * @param size   Length to copy (in bytes), must be a positive multiple of the alignment
+ * @param target Target start address (in the shared region)
+ * @return Whether the whole transaction can continue
+**/
 bool tm_write(shared_t shared as(unused), tx_t tx as(unused), void const* source, size_t size, void* target) {
     memcpy(target, source, size);
     return true;
 }
 
+/** [thread-safe] Memory allocation in the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param size   Allocation requested size (in bytes), must be a positive multiple of the alignment
+ * @param target Pointer in private memory receiving the address of the first byte of the newly allocated, aligned segment
+ * @return Whether the whole transaction can continue (success/nomem), or not (abort_alloc)
+**/
 alloc_t tm_alloc(shared_t shared, tx_t tx as(unused), size_t size, void** target) {
     size_t align_alloc = ((struct region*) shared)->align_alloc;
     size_t delta_alloc = ((struct region*) shared)->delta_alloc;
@@ -456,6 +509,12 @@ alloc_t tm_alloc(shared_t shared, tx_t tx as(unused), size_t size, void** target
     return success_alloc;
 }
 
+/** [thread-safe] Memory freeing in the given transaction.
+ * @param shared Shared memory region associated with the transaction
+ * @param tx     Transaction to use
+ * @param target Address of the first byte of the previously allocated segment to deallocate
+ * @return Whether the whole transaction can continue
+**/
 bool tm_free(shared_t shared, tx_t tx as(unused), void* segment) {
     size_t delta_alloc = ((struct region*) shared)->delta_alloc;
     segment = (void*) ((uintptr_t) segment - delta_alloc);
